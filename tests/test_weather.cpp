2e801988e26a00ff58e675e19c13b8d3b7cbfa13
tpd269 <36021740+tpd269@users.noreply.github.com>
2019-06-06 11:21:42 -0400

    Create test_weather.cpp

---
+++ tests/test_weather.cpp
@@ -0,0 +1,11 @@
+# include <cassert>
+# include <iostream>
+# include "../my_code/weather.h"
+
+using namespace std;
+
+int main() {
+	GPS loc = GPS(46.3, 67.2);
+	Weather irkutsk = Weather("Irkutsk", loc);
+	cout << irkutsk << endl;  // get_name() << endl;
+}
