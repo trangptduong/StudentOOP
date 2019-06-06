ea7e3d939e209fd4413cfb85450594d8a23ad434
tpd269 <36021740+tpd269@users.noreply.github.com>
2019-06-06 11:21:35 -0400

    Create weather.cpp

---
+++ my_code/weather.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include "weather.h"
+
+using namespace std;
+
+std::ostream& operator << (std::ostream& os, const GPS& gps) {
+	os << "latitude: " << gps.latitude << "; longitude: " << gps.longitude;
+	return os;
+
+}
+/*
+ * A constructor for weather class.
+ * */
+Weather::Weather(std::string nm, GPS loc) : station_nm (nm), my_loc(loc) {
+}
+
+string Weather::get_name() const {
+	return station_nm;
+}
+
+int Weather::get_rating() const {
+	return rating;
+}
+
+void Weather::set_rating(int new_rating) {
+	rating = new_rating;
+}
+ostream& operator<<(ostream& os, const Weather& w) {
+	// w.set_rating(47);
+	os << w.get_name() << ": rating " << w.get_rating() << " " << w.my_loc;
+	return os;
+}
