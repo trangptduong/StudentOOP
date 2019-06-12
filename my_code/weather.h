2dafbb1b9684fb1b5801f3267b1bdd3d3aa7f829
trangptduong <phuongthuytrangduong@gmail.com>
2019-06-12 12:55:42 -0400

    Update weather.h

--- my_code/weather.h
+++ my_code/weather.h
@@ -2,6 +2,40 @@
 # define WEATHER_H
 
 # include <string>
+# include <vector>
+
+/*
+*This will be the base class for all types of images.
+**/
+class Image {
+ public:
+	Image(int w, int h, std::string flnm);
+	// copy constructor
+	Image(const Image& img2);
+	~Image();
+	Image& operator=(const Image& img2);
+	int image_sz();
+
+	/*
+	* Setting `display() = 0` here makes this a abstract
+	* class that can't be implemented.
+	**/
+	std::string display(std::string s);
+	/*
+	 * If we don't want virtual method lookup, we
+	 * could just declare:
+	 * void display();
+	 * */
+
+	int get_height() { return height; }
+	int get_width() { return width; }
+ private:
+	int width;
+	int height;
+	std::string filename;
+	char* image_buf;
+	void copy_fields(const Image& img2);
+};
 
 struct GPS {
 	double latitude;
@@ -11,6 +45,34 @@ struct GPS {
 
 std::ostream& operator<<(std::ostream& os, const GPS& gps);
 
+class Date {
+	friend std::ostream& operator<<(std::ostream& os, const Date& date);
+ public:
+	Date(int d, int m, int y);
+ private:
+	int day;
+	int month;
+	int year;
+};
+
+class WReading {
+	friend std::ostream& operator<<(std::ostream& os, const WReading& wr);
+ public:
+	WReading(Date dt, double temp, double hum, double ws) :
+		date(dt), temperature(temp), humidity(hum), windspeed(ws)
+	{
+	}
+
+	double get_tempF();
+	double get_tempC() { return temperature; }
+
+ private: 
+	Date date;
+	double temperature;  // stored temp in C
+	double humidity;
+	double windspeed;
+};
+
 const int UNRATED = -1;
 const int BAD = 0;
 const int OK = 1;
@@ -19,12 +81,14 @@ const int GOOD = 2;
 class Weather {
 	// << should output name, rating, and use the GPS << to output my_loc
 	friend std::ostream& operator<<(std::ostream& os, const Weather& w);
-public:
+ public:
 	Weather(std::string nm, GPS loc);
 	std::string get_name() const;
 	int get_rating() const;
 	void set_rating(int new_rating);
-private:
+	void add_reading(WReading wr);
+ private:
+	std::vector <WReading> wreadings;
 	std::string station_nm;
 	GPS my_loc;
 	int rating = UNRATED;
