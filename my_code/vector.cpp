606d54b73221ee6fdd7e29c0144ff223dc2c0e0d
trangptduong <phuongthuytrangduong@gmail.com>
2019-06-25 12:22:12 -0400

    Update vector.cpp

--- my_code/vector.cpp
+++ my_code/vector.cpp
@@ -1,24 +1,28 @@
 # include <iostream>
 # include "vector.h"
-
+# include <algorithm>
 using namespace std;
 
 void print_vector(const MyVec& v) {
-	for (int i = 0; i < v.size(); i++) {
-		cout << v[i] << " ";
-	}
+	for (int i : v) cout << i << " ";
 	cout << endl;
 }
 
-MyVec::MyVec() {
-	sz = 0;
+MyVec::MyVec() : sz(0) {
 	capacity = DEF_CAPACITY;
 	data = new int[DEF_CAPACITY];
+}
+
+MyVec::MyVec(int sz, int val) : sz{sz} {
+	capacity = max(DEF_CAPACITY, sz * CAPACITY_MULT);  
+	data = new int[capacity];
+	for (size_t i = 0; i < sz; i++) data[i] = val;
 }
 
 MyVec::MyVec(const MyVec& v2) {
 	capacity = v2.capacity;
 	sz = v2.size();
+
 	data = new int[capacity];
 	// copy over existing data
 	for (int i = 0; i < size(); i++) {
@@ -89,12 +93,29 @@ bool operator==(MyVec& v1, MyVec& v2) {
 	if (v1.size() != v2.size()) {
 		return false;
 	} 
-	else {
+	else {  // v1.size() == v2.size()
 		for (int i = 0; i < v1.size();  i++) {
 			if (v1[i] != v2[i]) {
 				return false;
 			}
 		}
 		return true;
+	}
+}
+
+MyVec::Iterator MyVec::begin() const {
+	return MyVec::Iterator(data);
+}
+
+MyVec::Iterator MyVec::end() const {
+	return MyVec::Iterator(data + size());
+}
+
+void MyVec::copy(const MyVec& v2) {
+	sz = v2.sz;
+	capacity = v2.capacity;
+	data = new int[capacity];
+	for (int i = 0; i < sz; i++) {
+		data[i] = v2.data[i];
 	}
 }
